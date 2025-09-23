31fe6ab6e7e93148c60a143914f8f1d37ac1be0d
Lucine <93476156+Enicul@users.noreply.github.com>
2025-09-23 10:02:35 +0800

    Delete ana_demo directory

--- ana_demo/test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from analysts.fundamentals import get_fundamentals_func
-
-async def main():
-    result = await get_fundamentals_func("AAPL")
-    print(result)
-
-if __name__ == "__main__":
-    asyncio.run(main())
